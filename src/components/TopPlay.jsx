8862d078d8c9ee7a6605f0f57064226f693ed8b7
José Marinho <clowdcap@hotmail.com>
2022-09-29 14:00:50 -0300

    update sidebar

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,18 @@
+import { useEffect, useRef } from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import PlayPause from './PlayPause'
+import {  }
+
+import 'swiper/css'
+import 'swiper/css/free-mode'
+
+ 
+
 const TopPlay = () => (
   <div>TopPlay</div>
-);
+)
 
-export default TopPlay;
+export default TopPlay
